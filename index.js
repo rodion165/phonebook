abf3190d1734b23b67f4bad813faa22280718974
rodion165 <101893088+rodion165@users.noreply.github.com>
2023-03-27 14:24:33 +0500

    Add database support

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const Person = require('./moduls/persons')
+
 
 app.use(cors())
 app.use(express.json())
@@ -10,30 +13,11 @@ app.use(express.static('build'))
 
 
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let persons = []
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(notes => {
+    response.json(persons)
+  })
 })
 
 app.get("/info", (request, response) => {
@@ -42,48 +26,42 @@ app.get("/info", (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
-  
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id) 
   const person = persons.filter(person => person.id !== id)
+
   response.status(204).end()
 })
 
 const generateId = () => {
-  const id = Math.floor(MathRandom()*1000)
+  const id = Math.floor(Math.random()*1000)
   return id
 }
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!req.body.name || !req.body.number) {
-    return res.status(404).json({
+  if (!request.body.name || !request.body.number) {
+    return response.status(404).json({
       error: 'missing name or number'
     })
   }
-
-  const person = {
-    id: body.id,
+  const person = new Person({
+    id: generateId(),
     name: body.name,
     number: body.number
-  }
-  persons = persons.concat(person)
-
-  response.json(person)
+  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
-const PORT =process.env.PORT || 3001
-
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length>5) {
+  console.log('remove the first and last name in one quotation marks')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+
+const url =
+    `mongodb+srv://rodionkonst165:${password}@cluster0.h5zmluj.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person ({
+    name:process.argv[3],
+    number:process.argv[4]
+})
+person.save().then(result => {
+  console.log(`added ${person.name} ${person.number} to phonebook`)
+  mongoose.connection.close()
+})
